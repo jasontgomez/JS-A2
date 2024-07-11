ac1b84303d90dcdbcf5c37459a4947938828d1eb
Sebastian Bruce <sdbruce@proton.me>
2024-07-11 16:33:29 -0400

    converted array to object and updated logic

--- Assignment 2 - COMP1073/js/gallery.js
+++ Assignment 2 - COMP1073/js/gallery.js
@@ -1,11 +1,11 @@
-
-// array of images
-const imageArray = [
-    ['images/flowers-pink-small.jpg', 'these flowers are pink'], 
-    ['images/flowers-purple-small.jpg', 'these flowers are purple'], 
-    ['images/flowers-red-small.jpg', 'these flowers are red'], 
-    ['images/flowers-white-small.jpg', 'these flowers are white'], 
-    ['images/flowers-yellow-small.jpg', 'these flowers are yellow']]
+// object of images
+const imageObject = {
+    'images/flowers-pink-small.jpg': 'these flowers are pink', 
+    'images/flowers-purple-small.jpg': 'these flowers are purple', 
+    'images/flowers-red-small.jpg': 'these flowers are red', 
+    'images/flowers-white-small.jpg': 'these flowers are white', 
+    'images/flowers-yellow-small.jpg': 'these flowers are yellow'
+}
 
 // references
 const featureImage = document.querySelector('img');
@@ -13,10 +13,10 @@ const featureDesc = document.querySelector('figcaption');
 
 // add images to html
 const container = document.getElementById('image-container');
-for (let i = 0; i < imageArray.length; i++) {
+for (let key in imageObject) {
     const img = document.createElement('img');
-    img.src = imageArray[i][0];
-    if (i == 0) {
+    img.src = key;
+    if (container.children.length === 0) {
         img.classList.add('selected');
     }
     container.appendChild(img);
@@ -32,22 +32,20 @@ imageGallery.addEventListener('click', function(event){
     //get all selected imgs
     const selected = document.querySelectorAll(".selected");
 
-    //remove the selected id
+    //remove the selected class
     selected.forEach(cell => cell.classList.remove("selected"));
 
     console.log('Received ' + cleanSrc(event.target.src));
     updateFigure(cleanSrc(event.target.src));   
-    //add selected id to clicked img
+    //add selected class to clicked img
     event.target.classList.add('selected');
 
-  });
-
+});
 
  // function to clean src string, returns local path
 function cleanSrc(inputString) {
     return inputString.substring(inputString.search('images'));
 }
-
 
 // function to update big image
 function updateFigure(targetSrc) {
@@ -55,15 +53,13 @@ function updateFigure(targetSrc) {
     let updateImg = '';
     let updateDesc = '';
 
-    // loop through image array
-    for (let j = 0; j < imageArray.length; j++) {
-        if(imageArray[j][0] == targetSrc){
-            // new image path
-            updateImg = updateImg + imageArray[j][0].replace('small', 'large');
-            // new desc
-            updateDesc = updateDesc + imageArray[j][1];
-        }
+    if (imageObject[targetSrc]) {
+        // new image path
+        updateImg = targetSrc.replace('small', 'large');
+        // new desc
+        updateDesc = imageObject[targetSrc];
     }
+
     // update featured images
     featureImage.src = updateImg;
     
@@ -72,4 +68,4 @@ function updateFigure(targetSrc) {
 }
 
 // first element
-updateFigure(imageArray[0][0]); 
+updateFigure('images/flowers-pink-small.jpg');
